223450df7af77247e06cc6fb486dea2d9280b229
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-22 22:00:43 +1300

    create desktop sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,25 @@
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <>
+      <DesktopSidebar />
+      <MobileSidebar />
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
+
+
+const DesktopSidebar = () => {
+  return (
+    <div className="p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block">
+      <div className="flex flex-col gap-20 sticky top-10 left-0">
+        <div className="w-full">
+          <img src="/logo.svg" alt="logo" className="hidden md:block" />
+          <img src="/logo.svg" alt="logo" className="block md:hidden" />
+        </div>
+      </div>
+    </div>
+  );
+};
