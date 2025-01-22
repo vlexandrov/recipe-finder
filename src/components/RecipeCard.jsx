322b8de9b3225a40569fc82ed034068e93414a9a
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-23 05:14:16 +1300

    Update RecipeCard to render recipe data dynamically from meal object in API

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,16 +5,16 @@ const RecipeCard = ({meal}) => {
   return (
     <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
         {/* anchor - ensures entire thing (image) clickable to link; preview pic */}
-        <a href="#" className="relative h-32">
+        <a href={meal.strYoutube} className="relative h-32">
             {/* inside anchor, image then div over image in absolute position */}
-            <img src="/1.jpg" alt="recipe img"
+            <img src={meal.strMealThumb} alt="recipe img"
             className="rounded-md w-full h-full object-cover cursor-pointer"
             />
 
             {/* servings div */}
             <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex
             items-center gap-1 text-sm">
-            <Soup size={"16"} /> {meal.idMeal}
+            <Soup size={"16"} /> {meal.strCategory}
             </div>
 
             {/* favourites button div */}
@@ -25,18 +25,22 @@ const RecipeCard = ({meal}) => {
 
         {/* recipe text title */}
         <div className="flex mt-1">
-            <p className="font-bold tracking-wide">Roasted Chicken</p>
+            <p className="font-bold tracking-wide">{meal.strMeal}</p>
         </div>
 
         {/* cuisine */}
-        <p className="my-2">Turkish Kitchen</p>
+        <p className="my-2">{meal.strArea}</p>
 
         {/* tag thingies */}
         <div className="flex gap-2 mt-auto">
-            <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-                <HeartPulse size={"16"} />
-                <span className="text-sm tracking-tighter font-semibold">Gluten-free</span>
+            {/* if meal.strTags is not null then render */}
+            {meal.strTags && (
+                <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+                {/* <HeartPulse size={"16"} /> */}
+                <span className="text-sm tracking-tighter font-semibold">{meal.strTags}</span>
             </div>
+            )}
+            
             <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
                 <HeartPulse size={"16"} />
                 <span className="text-sm tracking-tighter font-semibold">Heart-healthy</span>
