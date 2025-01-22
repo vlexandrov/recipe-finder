d4db890433638036d700ee9dc754740dbc601638
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-23 00:21:29 +1300

    create mobile sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <>
       <DesktopSidebar />
-      {/*<MobileSidebar />*/}
+      <MobileSidebar />
     </>
   );
 };
@@ -33,6 +33,19 @@ const DesktopSidebar = () => {
           </Link>
         </ul>
       </div>
+    </div>
+  );
+};
+
+const MobileSidebar = () => {
+  return(
+    <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white z-10 p-2 sm:hidden">
+      <Link to={"/"}>
+        <Home size={"24"} className="cursor-pointer"/>
+      </Link>
+      <Link to={"/favourites"}>
+        <Heart size={"24"} className="cursor-pointer"/>
+      </Link>
     </div>
   );
 };
