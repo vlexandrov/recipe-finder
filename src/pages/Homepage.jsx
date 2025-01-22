7b5c310b04833e88734cb2e86f2b49fdeb6d6fd8
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-23 04:08:06 +1300

    code for fetching and setting recipes from themealdb api

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,34 @@
 //import React from 'react'
 import { Search } from "lucide-react";
 import RecipeCard from "../components/RecipeCard";
+import { useEffect, useState } from "react";
+
+const APP_ID = null;
+const APP_KEY = "1"
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/${APP_KEY}/search.php?s=${searchQuery}`);
+      const data = await res.json();
+      setRecipes(data.meals)
+    } catch (error) {
+      console.log(error.message)
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchRecipes("chicken");
+  }, []);
+
+
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
