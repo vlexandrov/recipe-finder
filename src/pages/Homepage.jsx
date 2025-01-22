d4fcef70bdb0c07804daf29c0e3c35ee472a331a
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-23 00:38:45 +1300

    create homepage searchbar, heading, grid

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,35 @@
 //import React from 'react'
+import { Search } from "lucide-react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form>
+          <label className="input shadow-md flex items-center gap-2">
+            <Search size={"24"} />
+            <input type="text" 
+            className="text-sm md:textarea-md grow"
+            placeholder="Imma let u cook g"
+            />
+          </label>
+        </form>
+
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular choices
+        </p>
+
+        {/* cards */}
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
