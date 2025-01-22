8309702a69b3a8d7d5ff8fcce699dcf26146aea1
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-22 22:20:37 +1300

    create sidebar links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 //import React from 'react'
+import { Link } from "react-router-dom";
+import { Home } from "lucide-react";
 
 const Sidebar = () => {
   return (
     <>
       <DesktopSidebar />
-      <MobileSidebar />
+      {/*<MobileSidebar />*/}
     </>
   );
 };
@@ -20,6 +22,18 @@ const DesktopSidebar = () => {
           <img src="/logo.svg" alt="logo" className="hidden md:block" />
           <img src="/logo.svg" alt="logo" className="block md:hidden" />
         </div>
+        <ul className="flex flex-col items-center md:items-start gap-8">
+          <Link to={"/"} className="flex gap-1">
+            <Home size={"24"}/>
+            <span className="font-bold hidden md:block">Home</span>
+          </Link>
+          <Link to={"/favourites"} className="flex gap-1">
+          <Heart size={"24"}/>
+            <span className="font-bold hidden md:block">Favourites</span>
+          </Link>
+
+        </ul>
+
       </div>
     </div>
   );
