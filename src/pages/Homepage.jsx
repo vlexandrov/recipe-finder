e18619f5a83f9bdb74649a7837e851a8c8d8715a
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-24 05:11:15 +1300

    updated home page to have search functionality

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -33,11 +33,16 @@ const HomePage = () => {
     fetchRecipes("chicken");
   }, []);
 
+  const handleSearchRecipe = (e) => {
+    e.preventDefault();
+    fetchRecipes(e.target[0].value);
+  }
+
 
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
-        <form>
+        <form onSubmit={handleSearchRecipe}>
           <label className="input shadow-md flex items-center gap-2">
             <Search size={"24"} />
             <input type="text" 
