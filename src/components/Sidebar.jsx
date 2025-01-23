6ebeda3485b91fcbb10abdcf0c1caaf016672b74
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-24 10:45:25 +1300

    fix image paths for deployment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,8 +19,8 @@ const DesktopSidebar = () => {
     <div className="p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block">
       <div className="flex flex-col gap-20 sticky top-10 left-0">
         <div className="w-full">
-          <img src="/logo.svg" alt="logo" className="hidden md:block" />
-          <img src="/mobile-logo.svg" alt="logo" className="block md:hidden" />
+          <img src="logo.svg" alt="logo" className="hidden md:block" />
+          <img src="mobile-logo.svg" alt="logo" className="block md:hidden" />
         </div>
         <ul className="flex flex-col items-center md:items-start gap-8">
           {/* <Link 
