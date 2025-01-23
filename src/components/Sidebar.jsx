4b9b11307268682a0945da2d2259d31c382a8452
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-24 06:02:09 +1300

    updated sidebar to reload upon clicking home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const DesktopSidebar = () => {
           <img src="/mobile-logo.svg" alt="logo" className="block md:hidden" />
         </div>
         <ul className="flex flex-col items-center md:items-start gap-8">
-          <Link to={"/"} className="flex gap-1">
+          <Link to={"/"} className="flex gap-1" onClick={() => window.location.reload()}>
             <Home size={"24"}/>
             <span className="font-bold hidden md:block">Home</span>
           </Link>
@@ -40,7 +40,7 @@ const DesktopSidebar = () => {
 const MobileSidebar = () => {
   return(
     <div className="flex justify-center gap-10 border-t fixed w-full bottom-0 left-0 bg-white z-10 p-2 sm:hidden">
-      <Link to={"/"}>
+      <Link to={"/"} onClick={() => window.location.reload()}>
         <Home size={"24"} className="cursor-pointer"/>
       </Link>
       <Link to={"/favourites"}>
