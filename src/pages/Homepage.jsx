6fa4c100153ac399bcbfd2eb14dddf995b0718b6
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-24 04:30:12 +1300

    update skeleton loading to look like content (prev used daisyui default)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -64,16 +64,14 @@ const HomePage = () => {
           ))}
 
           {loading &&
-            [...Array(9)].map((_, index) => (
-              <div className="flex w-52 flex-col gap-4">
-                <div className="flex items-center gap-4">
-                  <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
-                  <div className="flex flex-col gap-4">
-                    <div className="skeleton h-4 w-20"></div>
+            [...Array(15)].map((_, index) => (
+              <div key={index} className="flex w-full flex-col gap-4">
+                  <div className="skeleton h-32 w-full"></div>
+                  <div className="flex justify-between">
                     <div className="skeleton h-4 w-28"></div>
-                  </div>
-                </div>
-                <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-24"></div>
+                  </div>                
+                <div className="skeleton h-4 w-1/2"></div>
               </div>
             ))}
 
