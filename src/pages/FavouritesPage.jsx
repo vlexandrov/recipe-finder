0fadf9af9dc58915fba9a1f0ddffe4175afcdf8d
vlexandrov <101042522+vlexandrov@users.noreply.github.com>
2025-01-24 04:56:14 +1300

    updated favourites page to handle favourite functionality and display favourites

--- src/pages/FavouritesPage.jsx
+++ src/pages/FavouritesPage.jsx
@@ -3,28 +3,25 @@
 import RecipeCard from "../components/RecipeCard";
 
 const FavouritesPage = () => {
-  const fav = false;
+  const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
 
   return (
     <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
       <div className="max-w-screen-lg mx-auto">
         <p className="font-bold text-3xl md:text-5xl my-4">My Favourites</p>
 
-        {!fav && (
+        {favourites.length === 0 && (
           <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="/404.svg" className="h-3/4" alt="404 svg" />
           </div>
         )}
-
-        {fav && (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-          </div>
-        )}
-
+       
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {favourites.map((meal) => (
+            <RecipeCard key={meal.strMeal} meal={meal}/>
+          ))}
+        </div>
+        
       </div>
     </div>
   )
